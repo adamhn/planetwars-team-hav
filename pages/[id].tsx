a1aadd01d9cae41ca0ff5975b6f459d9eb500578
Vebjørn Kvisli <vebjorn.kvisli@gjensidige.no>
2022-10-19 09:32:13 +0200

    add planet details

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,67 +1,65 @@
-import { Container, Col, Row } from '@gjensidige/nci-core-grid/lib';
-import { Title } from '@gjensidige/nci-core-typography/lib';
-import { Text } from '@gjensidige/nci-core-typography/lib';
-import { getAllPlanets } from '../utils/api';
+import { Container, Col, Row } from "@gjensidige/nci-core-grid/lib";
+import { Title } from "@gjensidige/nci-core-typography/lib";
+import { Text } from "@gjensidige/nci-core-typography/lib";
+import { getAllPlanets } from "../utils/api";
 import { Planet } from "../utils/types";
 
 type Props = {
-    errors?: string
-    planet?: Planet;
-}
+  errors?: string;
+  planet?: Planet;
+};
 
 const StaticPropsDetail = ({ planet, errors }: Props) => {
-    if (errors) {
-        return (
-            <div title="Planet">
-                <p>
-                    <span style={{ color: 'red' }}>Error:</span> {errors}
-                </p>
-            </div>
-        )
-    }
-
-    console.log("Planet " + planet);
+  if (errors) {
     return (
-        <Container>
-            <Row>
-                <Col xxs={{ span: 5, offset: 3 }}>
-                    <Title tag="h1">Details</Title>
-                    <Text>Gravity: </Text>
-                    <Text>Population: </Text>
-                    <Text>Terrains: </Text>
-                    <Text>Diameter: </Text>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+      <div title="Planet">
+        <p>
+          <span style={{ color: "red" }}>Error:</span> {errors}
+        </p>
+      </div>
+    );
+  }
 
-export default StaticPropsDetail
+  console.log("Planet " + planet);
+  return (
+    <Container>
+      <Row>
+        <Col xxs={{ span: 5, offset: 3 }}>
+          <Title tag="h1">{planet?.name}</Title>
+          <Text>
+            Gravity: <strong>{planet?.gravity}</strong>
+          </Text>
+          <Text>
+            Population: <strong>{planet?.population}</strong>
+          </Text>
+          <Text>
+            Terrains: <strong>{planet?.terrains.join(", ")}</strong>
+          </Text>
+          <Text>
+            Diameter: <strong>{planet?.diameter}</strong>
+          </Text>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
+export default StaticPropsDetail;
 
-
-
-export async function getStaticPaths() {
-
-    const data = await getAllPlanets()
+// This gets called on every request
+export async function getServerSideProps(context: any) {
+  const id = context.params.id; // Get ID from slug `/book/1`
+  // Fetch data from external API
+  console.log(id);
+  try {
+    const data = await getAllPlanets();
     const planets = data.data.allPlanets.planets as Planet[];
-    const paths = planets.map((planet) => ({
-        params: { id: planet.id },
-    }))
-    return {
-        paths: paths,
-        fallback: false, // can also be true or 'blocking'
-    }
-}
+    const planet = planets.find((p) => p.id === id);
+    return { props: { planet } };
+  } catch (error) {
+    console.error("could not fetch planets", error);
+  }
 
-
-
-
-// `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps() {
-
-    return {
-        // Passed to the page component as props
-        props: { planet: {} },
-    }
+  // Pass data to the page via props
+  return { props: null };
 }
